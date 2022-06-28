47affb7fa88be241cdc18060d7d0d63edaa1af1f
Alan Jones <aljones1816@gmail.com>
2022-06-28 10:37:04 -0400

    add todo card and project card UI modules

---
+++ src/Components/UI/projectUI.ts
@@ -0,0 +1,4 @@
+// a div with a single project card
+// clicking project card should filter todos to only those that belong to that project
+// card needs a button to delete the project
+// card needs a button to edit the project name
---
+++ src/Components/UI/toDoCard.ts
@@ -0,0 +1,4 @@
+// Should be a card-like component that displays a todo object - todo object will be input
+// Should have a button that deletes the todo
+// Should have a button that marks the todo as complete
+// Should have a button to edit the ToDo
